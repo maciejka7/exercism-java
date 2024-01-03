05e67a130dbc46be5a976e5a7b4a21aa42fbd6e0
Michał Maciejewski <maciejewskimichal7@gmail.com>
2024-01-03 11:59:58 +0000

    solutions to new exercise

---
+++ exercism-lerning/src/main/java/annalynsInfiltration/AnnalynsInfiltration.java
@@ -0,0 +1,19 @@
+package annalynsInfiltration;
+
+class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        return !knightIsAwake;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return archerIsAwake || prisonerIsAwake || knightIsAwake;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        return prisonerIsAwake && !archerIsAwake;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        return (prisonerIsAwake && !archerIsAwake && !knightIsAwake) || (!archerIsAwake && petDogIsPresent);
+    }
+}
---
+++ exercism-lerning/src/main/java/carsAssemble/CarsAssemble.java
@@ -0,0 +1,28 @@
+package carsAssemble;
+
+public class CarsAssemble {
+    private final double productionCapacityPerHour = 221;
+    
+    public double productionRatePerHour(int speed) {
+        return (speed * this.productionCapacityPerHour) * this.getSuccedRate(speed); 
+
+    }
+
+    public int workingItemsPerMinute(int speed) {
+        return (int)this.productionRatePerHour(speed) / 60;
+    }
+
+    private double getSuccedRate(int speed) {
+       if (speed == 0) {
+            return 0;
+        } else if (speed <= 4) {
+            return 1;
+        } else if (speed > 4 && speed <= 8) {
+            return 0.9;
+        } else if (speed == 9) {
+            return 0.8;
+        } else {
+            return 0.77;
+        }
+    }
+}
